3dabf66858a601f254aabd91f42c72b17bac3ecc
Garry Yao <yaojun85@gmail.com>
2014-09-15 00:30:34 +0800

    introduce low volume perception

--- examples/widget/volume.js
+++ examples/widget/volume.js
@@ -9,20 +9,32 @@ define([
 ], function (Widget, c3, _) {
   'use strict';
 
-  var FREQ = 1000;
+  var FREQ = 2500;
 
   /**
    * widget visualize recording volume with a animated line chart
    */
   return Widget.extend({
-    column: function () {
+    column: function getColumnDate() {
       if(!this.volumes.length)
         return null;
 
+      var me = this;
+      var frames = this.volumes.slice(this.index);
+      this.index = this.volumes.length;
+      var avgs = _.range(0, frames.length).map(function () { return me.avgVolume || 0; });
       return [
-        ['ellapsed'].concat(_.pluck(this.volumes, 'ellapsed')),
-        ['volume'].concat(_.pluck(this.volumes, 'volume'))
+        ['ellapsed'].concat(_.pluck(frames, 'ellapsed')),
+        ['volume'].concat(_.pluck(frames, 'volume')),
+        ['avg'].concat(avgs)
       ];
+    },
+    avg: function () {
+      var frames = this.volumes.slice(this.avgIndex);
+      this.avgVolume  = _.reduce(frames, function (sum, data) {
+        return sum + data.volume;
+      }, 0) / frames.length;
+      this.avgIndex = this.volumes.length;
     },
     flow: function () {
       var me = this;
@@ -38,17 +50,27 @@ define([
           }
         });
         me.flowing = 1;
-        me.length = me.volumes.length;
-        me.volumes = [];
       }
     },
     'hub/recorder/record/started': function () {
+      clearInterval(this.interval_avg);
+      delete this.avgVolume;
+      this.index = 0;
+      this.avgIndex = 0;
+
       this.chart = c3.generate({
         bindTo: '#chart',
         data: {
           x: 'ellapsed',
           columns: [],
-          type: 'spline'
+          type: 'spline',
+          types: {
+            'avg': 'step'
+          },
+          names: {
+            volume: 'Volume',
+            avg: 'Avg. Volume'
+          }
         },
         axis: {
           y: {
@@ -64,6 +86,7 @@ define([
           }
         }
       });
+      this.interval_avg = setInterval(this.avg.bind(this), FREQ);
       this.volumes = [];
     },
     'hub/recorder/record/progress': function (ellapsed, volume) {
